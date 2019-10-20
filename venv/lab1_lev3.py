559f2df8b0c194dab1f0e44802a666efe66c14c7
alexlapinskiy <olexsandr.lapinskiy@gmail.com>
2019-10-20 16:01:53 +0300

    1 lab 3 level

---
+++ venv/lab1_lev3.py
@@ -0,0 +1,16 @@
+number=input("Input last 3 digits of the scoring book: ")
+def convert_base(num, to_base=10, from_base=10):
+    # first convert to decimal number
+    if isinstance(num, str):
+        n = int(num, from_base)
+    else:
+        n = int(num)
+    # now convert decimal to 'to_base' base
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < to_base:
+        return alphabet[n]
+    else:
+        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
+print(number," in decimal -> ",convert_base(number, from_base=8, to_base=10))
+print(number,"in binary -> ",convert_base(number, from_base=8, to_base=2))
+print(number,"in hex -> ",convert_base(number, from_base=8, to_base=16))
